96621f90367af196f4492398579098a2e47b3935
Nakul Chauhan <nakulchauhan@cppib.com>
2023-03-18 09:46:44 +0530

    Create SmallFactorials.java

---
+++ src/SmallFactorials.java
@@ -0,0 +1,23 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class SmallFactorials {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testCases = Integer.parseInt(sc.nextLine());
+
+        while (testCases > 0) {
+            System.out.println(smallFactorial(sc.nextInt()));
+            testCases--;
+        }
+    }
+
+    private static BigInteger smallFactorial(int n) {
+        BigInteger fact = BigInteger.ONE;
+        for (int i = 2; i <= n; i++) {
+            fact = fact.multiply(BigInteger.valueOf(i));
+        }
+
+        return fact;
+    }
+}
