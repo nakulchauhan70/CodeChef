276c6c401d25c7b608cee52febe784d057ca36f9
Nakul Chauhan <nkul.ic20@gmail.com>
2023-03-18 15:25:28 +0530

    Create KitchenTimetable.java

---
+++ src/beginners/KitchenTimetable.java
@@ -0,0 +1,33 @@
+package beginners;
+
+import java.util.Scanner;
+
+public class KitchenTimetable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testCases = Integer.parseInt(sc.nextLine());
+
+        while (testCases > 0) {
+            int students = sc.nextInt();
+            int[] moments = {sc.nextInt(), sc.nextInt(), sc.nextInt()};
+            int[] timeRequired = {sc.nextInt(), sc.nextInt(), sc.nextInt()};
+            System.out.println(kitchenTimetable(moments, timeRequired));
+            testCases--;
+        }
+    }
+
+    private static int kitchenTimetable(int[] moments, int[] timeRequired) {
+        int a = 0;
+        int count = 0;
+
+        for (int i = 0; i < moments.length; i++) {
+            if (moments[i] - a >= timeRequired[i]) {
+                count++;
+            }
+
+            a = moments[i];
+        }
+
+        return count;
+    }
+}
