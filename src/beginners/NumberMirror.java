88dfaa037bb5d18ef076d64f3741f79ed3afd53e
Nakul Chauhan <nakulchauhan@cppib.com>
2023-03-26 09:58:50 +0530

    Create NumberMirror.java

---
+++ src/beginners/NumberMirror.java
@@ -0,0 +1,10 @@
+package beginners;
+
+import java.util.Scanner;
+
+public class NumberMirror {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt());
+    }
+}
